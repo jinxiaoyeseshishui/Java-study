03d0cfcbd3b7ada6b49104f2a5bd1d0e30a95de3
Your Name <youremail@yourdomain.com>
2026-04-20 00:16:26 +0800

    HelloWorld练习

--- src/Helloworld/helloworld.java
+++ src/Helloworld/helloworld.java
@@ -2,6 +2,6 @@ package Helloworld;
 
 public class helloworld {
     public static void main(String[] args) {
-        System.out.println("Hello World！");
+        System.out.println("Hello World!");
     }
 }
